ecbaa6bb2c5d07e4ad388febd82eb743f305b2c9
Linqizhi-hue <3310863697@qq.com>
2026-07-11 17:38:52 +0800

    后端依赖添加

---
+++ backend/backend-Lin/src/main/java/com/uestc/lin/backendlin/BackendLinApplication.java
@@ -0,0 +1,13 @@
+package com.uestc.lin.backendlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLinApplication.class, args);
+    }
+
+}
---
+++ backend/backend-Lin/src/test/java/com/uestc/lin/backendlin/BackendLinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uestc.lin.backendlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
